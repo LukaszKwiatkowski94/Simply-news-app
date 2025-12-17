d205bb059c770e3430857665f6efa61513d90326
LukaszKwiatkowski94 <kwiatkowskilukasz1994@gmail.com>
2025-12-17 21:39:54 +0100

    Fixes

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -10,60 +10,48 @@ use APP\Models\AbstractModel;
 use Exception;
 use PDO;
 
-class UserModel extends AbstractModel{
+class UserModel extends AbstractModel
+{
 
     public function create(array $data)
     {
-        try
-        {
-            if($data['username'] == '' || $data['password'] == '' || $data['name'] == '' || $data['surname'] == '')
-            {
-                throw new UserException("Incomplete user creation data. | Database error.",400);
+        try {
+            if ($data['username'] == '' || $data['password'] == '' || $data['name'] == '' || $data['surname'] == '') {
+                throw new UserException("Incomplete user creation data. | Database error.", 400);
             }
             $username = $this->connection->quote($data['username']);
-            $password = $this->connection->quote(password_hash($data['password'],PASSWORD_DEFAULT));
+            $password = $this->connection->quote(password_hash($data['password'], PASSWORD_DEFAULT));
             $name = $this->connection->quote($data['name']);
             $surname = $this->connection->quote($data['surname']);
-            $query = "INSERT into users(username,password,name,surname) VALUES($username,$password,$name,$surname)";
+            $query = "INSERT into SN_users(username,password,name,surname) VALUES($username,$password,$name,$surname)";
             $result = $this->connection->exec($query);
             return $result;
-        }
-        catch(Exception $e)
-        {
-            throw new UserException("User creation error. | Database error.",400);
+        } catch (Exception $e) {
+            throw new UserException("User creation error. | Database error.", 400);
         }
     }
 
     public function get(array $data)
     {
-        try
-        {
+        try {
             $username = $this->connection->quote($data['username']);
             $password = $data['password'];
             $query = "SELECT * 
-                    FROM users 
+                    FROM SN_users 
                     WHERE username = $username";
             $user = $this->connection->query($query);
             $getUser = $user->fetch(PDO::FETCH_ASSOC) ?? [];
-            if (isset($getUser))
-            {
-                if (password_verify($password, $getUser['password']))
-                {
+            if (isset($getUser)) {
+                if (password_verify($password, $getUser['password'])) {
                     return $getUser;
-                }
-                else
-                {
+                } else {
                     return [];
                 }
-            }
-            else
-            {
+            } else {
                 return [];
-            } 
-        }
-        catch(Exception $e)
-        {
-            throw new UserException("Error while logging in. Check the correctness of the data and log in again. | DataBase Error",400);
+            }
+        } catch (Exception $e) {
+            throw new UserException("Error while logging in. Check the correctness of the data and log in again. | DataBase Error", 400);
         }
     }
 }
--- templates/base.php
+++ templates/base.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title><?php echo $params['header'] ?? 'Simply News App'; ?></title>
+	<title><?php echo $params['header'] ? 'Simply News App - ' . $params['header'] : 'Simply News App'; ?></title>
 	
 	<!-- Preconnect for faster font loading -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
