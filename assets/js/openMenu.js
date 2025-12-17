4ba43362a5ea7da354690959856ac4875f652392
LukaszKwiatkowski94 <kwiatkowskilukasz1994@gmail.com>
2025-12-17 21:04:24 +0100

    New Templates - fixes

--- assets/js/openMenu.js
+++
@@ -1,5 +0,0 @@
-let btnMenu = document.querySelector(".nav__btn");
-let navList = document.querySelector(".nav__list");
-btnMenu.addEventListener("click", () => {
-    navList.classList.toggle('nav__list--mobile');
-});
