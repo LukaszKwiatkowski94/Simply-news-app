2eca6f8eebbaf999c6a80ca4d3c12f0fba2a77ee
LukaszKwiatkowski94 <kwiatkowskilukasz1994@gmail.com>
2025-12-20 00:49:44 +0100

    Category class

--- src/Classes/Category.php
+++ src/Classes/Category.php
@@ -3,3 +3,46 @@
 declare(strict_types=1);
 
 namespace APP\Classes;
+
+final class Category extends AbstractClass
+{
+    private int $id;
+    private string $name;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function fromArray(array $data): self
+    {
+        return new self(
+            $data['id'] ?? 0,
+            $data['name'] ?? ''
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+        ];
+    }
+
+    public function toJson(): string
+    {
+        return json_encode($this->toArray());
+    }
+}
