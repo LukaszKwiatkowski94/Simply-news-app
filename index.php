e0a54b574b306a1f9a8980a01ccc17e79f79786b
LukaszKwiatkowski94 <kwiatkowskilukasz1994@gmail.com>
2025-12-19 10:47:41 +0100

    routes

--- index.php
+++ index.php
@@ -12,92 +12,111 @@ use APP\Controllers\CommentsController;
 use APP\Controllers\ErrorController;
 use APP\Controllers\NewsController;
 use APP\Controllers\UserController;
+use APP\HttpMethod;
 use APP\Models\AbstractModel;
 use APP\Request;
+use APP\Router;
 
 try {
     $configuration = require_once("./config/config.php");
 
     AbstractModel::configuration($configuration);
 
-    $klein = new \Klein\Klein();
-    // NEWS
 
-    $klein->respond('GET', '/', function () {
+    // require_once __DIR__ . '/src/routes.php';
+
+    $router = new Router();
+
+    $router->add(HttpMethod::GET, '/', function () {
         $request = new Request($_GET, $_POST);
         AbstractController::$myPage = 'mainPage';
         (new NewsController($request))->run();
     });
 
-    $klein->respond('GET', '/news-show/[:id]', function ($req) {
-        $_GET['id'] = $req->id;
+    $router->add(HttpMethod::GET, '/news-show/{id}', function ($id) {
+        $_GET['id'] = $id;
         $request = new Request($_GET, $_POST);
         AbstractController::$myPage = 'show';
         (new NewsController($request))->run();
     });
 
-    $klein->respond(array('GET', 'POST'), '/news-create', function ($req, $response) {
+    $router->add(HttpMethod::GET, '/news-create', function () {
         $request = new Request($_GET, $_POST);
         AbstractController::$myPage = 'create';
-        AbstractController::$response = $response;
         (new NewsController($request))->run();
     });
 
-    $klein->respond('GET', '/news-list', function () {
+    $router->add(HttpMethod::POST, '/news-create', function () {
+        $request = new Request($_GET, $_POST);
+        AbstractController::$myPage = 'create';
+        (new NewsController($request))->run();
+    });
+
+    $router->add(HttpMethod::GET, '/news-list', function () {
         $request = new Request($_GET, $_POST);
         AbstractController::$myPage = 'list';
         (new NewsController($request))->run();
     });
 
-    $klein->respond('GET', '/news-delete/[:id]', function ($req, $response) {
-        $_GET['id'] = $req->id;
+    $router->add(HttpMethod::GET, '/news-delete/{id}', function ($id) {
+        $_GET['id'] = $id;
         $request = new Request($_GET, $_POST);
         AbstractController::$myPage = 'delete';
-        AbstractController::$response = $response;
         (new NewsController($request))->run();
     });
 
-    $klein->respond(array('GET', 'POST'), '/news-edit/[:id]', function ($req, $response) {
-        $_GET['id'] = $req->id;
+    $router->add(HttpMethod::GET, '/news-edit/{id}', function ($id) {
+        $_GET['id'] = $id;
         $request = new Request($_GET, $_POST);
         AbstractController::$myPage = 'edit';
-        AbstractController::$response = $response;
         (new NewsController($request))->run();
     });
 
-    // USER
+    $router->add(HttpMethod::POST, '/news-edit/{id}', function ($id) {
+        $_GET['id'] = $id;
+        $request = new Request($_GET, $_POST);
+        AbstractController::$myPage = 'edit';
+        (new NewsController($request))->run();
+    });
 
-    $klein->respond(array('GET', 'POST'), '/login', function ($req, $response) {
+    $router->add(HttpMethod::GET, '/login', function () {
         $request = new Request($_GET, $_POST);
         AbstractController::$myPage = 'login';
-        AbstractController::$response = $response;
         (new UserController($request))->run();
     });
 
-    $klein->respond(array('GET', 'POST'), '/signup', function ($req, $response) {
+    $router->add(HttpMethod::POST, '/login', function () {
+        $request = new Request($_GET, $_POST);
+        AbstractController::$myPage = 'login';
+        (new UserController($request))->run();
+    });
+
+    $router->add(HttpMethod::GET, '/signup', function () {
         $request = new Request($_GET, $_POST);
         AbstractController::$myPage = 'signup';
-        AbstractController::$response = $response;
         (new UserController($request))->run();
     });
 
-    $klein->respond('GET', '/logout', function ($req, $response) {
+    $router->add(HttpMethod::POST, '/signup', function () {
+        $request = new Request($_GET, $_POST);
+        AbstractController::$myPage = 'signup';
+        (new UserController($request))->run();
+    });
+
+    $router->add(HttpMethod::GET, '/logout', function () {
         $request = new Request($_GET, $_POST);
         AbstractController::$myPage = 'logout';
-        AbstractController::$response = $response;
         (new UserController($request))->run();
     });
 
-    // COMMENTS
-
-    $klein->respond('GET', '/comments-news/[:id]', function ($req, $response) {
-        $_GET['id'] = $req->id;
+    $router->add(HttpMethod::GET, '/comments-news/{id}', function ($id) {
+        $_GET['id'] = $id;
         $request = new Request($_GET, $_POST);
         AbstractController::$myPage = 'getComments';
         (new CommentsController($request))->run();
     });
 
-    $klein->respond('POST', '/comments-create', function ($req, $response) {
+    $router->add(HttpMethod::POST, '/comments-create', function () {
         $body = json_decode(file_get_contents('php://input'));
         $_POST['news'] = $body->news;
         $_POST['content'] = $body->content;
@@ -106,14 +125,7 @@ try {
         (new CommentsController($request))->run();
     });
 
-    // ERROR
-
-    $klein->onHttpError(function ($code, $router) {
-        AbstractController::$myPage = 'pageNotFound';
-        (new ErrorController(''))->run();
-    });
-
-    $klein->dispatch();
+    $router->dispatch();
 } catch (Exception $e) {
     AbstractController::$myPage = 'error';
     (new ErrorController($e->getMessage()))->run();
