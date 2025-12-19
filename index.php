6756f6d417c795ede630b4d86c2b1b0f18a86293
LukaszKwiatkowski94 <kwiatkowskilukasz1994@gmail.com>
2025-12-19 12:34:50 +0100

    new routing

--- index.php
+++ index.php
@@ -12,30 +12,12 @@ use APP\Controllers\ErrorController;
 use APP\Controllers\NewsController;
 use APP\Controllers\UserController;
 use APP\HttpMethod;
-use APP\Models\AbstractModel;
-use APP\Request;
 use APP\Router;
 
 try {
-
-    // require_once __DIR__ . '/src/routes.php';
-
     $router = new Router();
-    $router->add(HttpMethod::GET, '/', NewsController::class, 'mainPage');
-    $router->add(HttpMethod::GET, '/news-show/{id}', NewsController::class, 'show');
-    $router->add(HttpMethod::GET, '/news-create', NewsController::class, 'create');
-    $router->add(HttpMethod::POST, '/news-create', NewsController::class, 'create');
-    $router->add(HttpMethod::GET, '/news-list', NewsController::class, 'list');
-    $router->add(HttpMethod::GET, '/news-delete/{id}', NewsController::class, 'delete');
-    $router->add(HttpMethod::GET, '/news-edit/{id}', NewsController::class, 'edit');
-    $router->add(HttpMethod::POST, '/news-edit/{id}', NewsController::class, 'edit');
-    $router->add(HttpMethod::GET, '/login', UserController::class, 'logIn');
-    $router->add(HttpMethod::POST, '/login', UserController::class, 'logIn');
-    $router->add(HttpMethod::GET, '/signup', UserController::class, 'signUp');
-    $router->add(HttpMethod::POST, '/signup', UserController::class, 'signUp');
-    $router->add(HttpMethod::GET, '/logout', UserController::class, 'logOut');
-    $router->add(HttpMethod::GET, '/comments-news/{id}', CommentsController::class, 'getComments');
-    $router->add(HttpMethod::POST, '/comments-create', CommentsController::class, 'createComment');
+    $registerRoutes = require __DIR__ . '/config/routes.php';
+    $registerRoutes($router);
     $router->dispatch();
 } catch (Exception $e) {
     AbstractController::$myPage = 'error';
